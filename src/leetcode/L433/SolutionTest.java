d07ab3a5562e4b75801e5434a3a031f27724e6b7
kllcoo1300 <kllcoo1300@gmail.com>
2022-11-03 00:37:01 +0800

    2022/11/02 [Daily] 433. Minimum Genetic Mutation

---
+++ src/leetcode/L433/SolutionTest.java
@@ -0,0 +1,39 @@
+package leetcode.L433;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class SolutionTest {
+    @Test
+    public void case1() {
+        Solution solution = new Solution();
+        Assertions.assertEquals(
+                solution.minMutation(
+                        "AACCGGTT",
+                        "AACCGGTA",
+                        new String[]{"AACCGGTA"})
+                , 1);
+    }
+
+    @Test
+    public void case2() {
+        Solution solution = new Solution();
+        Assertions.assertEquals(
+                solution.minMutation(
+                        "AACCGGTT",
+                        "AAACGGTA",
+                        new String[]{"AACCGGTA","AACCGCTA","AAACGGTA"})
+                , 2);
+    }
+
+    @Test
+    public void case3() {
+        Solution solution = new Solution();
+        Assertions.assertEquals(
+                solution.minMutation(
+                        "AACCGGTT",
+                        "AACCGCTA",
+                        new String[]{"AACCGGTA","AACCGCTA","AAACGGTA"})
+                , 2);
+    }
+}

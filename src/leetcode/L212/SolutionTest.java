a042dabae59c5e0e86914650dfcf9dae678a6305
kllcoo1300 <kllcoo1300@gmail.com>
2022-11-07 22:13:41 +0800

    2022/11/05 [Daily] 212. Word Search II

---
+++ src/leetcode/L212/SolutionTest.java
@@ -0,0 +1,18 @@
+package leetcode.L212;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+class SolutionTest {
+    @Test
+    public void case1() {
+        Solution solution = new Solution();
+        Assertions.assertLinesMatch(solution.findWords(
+                        new char[][]{{'o', 'a', 'a', 'n'}, {'e', 't', 'a', 'e'}, {'i', 'h', 'k', 'r'}, {'i', 'f', 'l', 'v'}},
+                        new String[]{"oath", "pea", "eat", "rain"}),
+                Arrays.asList("eat", "oath"));
+    }
+
+}

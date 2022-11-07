5008faf669d6a471ee63b6bbe75d968b2317dd76
kllcoo1300 <kllcoo1300@gmail.com>
2022-11-07 22:22:00 +0800

    2022/11/07 [Daily] 1323. Maximum 69 Number

---
+++ src/leetcode/L1323/SolutionTest.java
@@ -0,0 +1,13 @@
+package leetcode.L1323;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class SolutionTest {
+    @Test
+    public void case1() {
+        Solution solution = new Solution();
+        Assertions.assertEquals(solution.maximum69Number(9669), 9969);
+    }
+
+}

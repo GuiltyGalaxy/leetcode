2ceff5d9275877e026dff6252feeb696b2a49da7
kllcoo1300 <kllcoo1300@gmail.com>
2023-02-15 21:43:42 +0800

    2023/02/15 [Top] 424. Longest Repeating Character Replacement

--- src/top/L424/Solution.java
+++ src/top/L424/Solution.java
@@ -1,26 +1,29 @@
 package top.L424;
 
+
 class Solution {
     public int characterReplacement(String s, int k) {
 
         int len = s.length();
-        int[] count = new int[26];
+        int[] count = new int[256];
         int start = 0;
         int maxCount = 0;
         int maxLength = 0;
 
         for (int end = 0; end < len; end++) {
             int curLen = end - start + 1;
-            //取得目前出現次數最多的字母
-            maxCount = Math.max(maxCount, ++count[s.charAt(end) - 'A']);
-            //如果window長度減去maxCount還大於k，代表window目前不符合條件
+            //累加目前window內count
+            count[s.charAt(end)]++;
+            //判斷目前加的這個字母是否有超過maxCount
+            maxCount = Math.max(maxCount, count[s.charAt(end)]);
+            //如果window長度減去maxCount大於k，代表window目前不符合條件
             //推移start到符合條件為止
             while (curLen - maxCount > k) {
-                count[s.charAt(start) - 'A']--;
+                count[s.charAt(start)]--;
                 start++;
                 curLen--;
             }
-            //取當前長度
+            //更新maxLength
             maxLength = Math.max(maxLength, curLen);
         }
         return maxLength;

edcc092688e5be2eeae37a5f7d169a8a539bea48
kllcoo1300 <kllcoo1300@gmail.com>
2023-02-25 14:54:45 +0800

    2023/02/25 [Top] 4. Median of Two Sorted Arrays

--- src/top/L4/Solution.java
+++ src/top/L4/Solution.java
@@ -12,22 +12,24 @@ public class Solution {
     }
 
     /**
-     * @param k 數組切點
+     * 取得兩數組中排第Kth的元素
      */
     private double getkth(int[] A, int aStart, int[] B, int bStart, int k) {
-        //超過邊界的情況下代表該數組內沒有任何數字為中位數，回傳另一數組的中位數
+        //超過邊界的情況下代表該數組內沒有Kth元素存在，回傳另一數組目前的Kth元素
         if (aStart > A.length - 1) return B[bStart + k - 1];
         if (bStart > B.length - 1) return A[aStart + k - 1];
         //k==1代表沒辦法再切了，回傳數組比較小的
         if (k == 1) return Math.min(A[aStart], B[bStart]);
 
+        //start為偏移量，故start + k / 2 - 1為目標中數位子
         int aIdx = aStart + k / 2 - 1;
         int bIdx = bStart + k / 2 - 1;
-
+        //取得中位數
         int aMid = aIdx < A.length ? A[aIdx] : Integer.MAX_VALUE;
         int bMid = bIdx < B.length ? B[bIdx] : Integer.MAX_VALUE;
 
-        //依照兩數組中位數情況，k - k / 2為切點繼續去找
+        //比較小的中位數數組則將k/2前的數移除不做處理
+        //也就是start=index+1，並且目標Kth = k - k/2
         if (aMid < bMid)
             return getkth(A, aIdx + 1, B, bStart, k - k / 2);
         else

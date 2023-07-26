ace4cc4ee23510d171fd79e83f17667a859203c5
kllcoo1300 <kllcoo1300@gmail.com>
2023-07-27 03:23:12 +0800

    2023/07/27 [Top] 146. LRU Cache

--- src/top/L146/LRUCache.java
+++ src/top/L146/LRUCache.java
@@ -6,28 +6,29 @@ import java.util.Map;
 public class LRUCache {
 
     //map是用來記錄key,val
-    private Map<Integer, DLinkedNode> cache = new HashMap<>();
+    private Map<Integer, Node> cache;
     //DLinkedNode是用來記錄目前常用排序情況
     //head, tail都是假指標，用來指向目前的head與tail
-    private DLinkedNode head, tail;
+    private Node head;
+    private Node tail;
     private int currSize;
     private int maxSize;
 
-    public LRUCache(int maxSize) {
-        this.currSize = 0;
-        this.maxSize = maxSize;
+    public LRUCache(int capacity) {
+        cache = new HashMap<>();
+        currSize = 0;
+        maxSize = capacity;
 
-        head = new DLinkedNode();
+        head = new Node();
+        tail = new Node();
+
         head.prev = null;
-
-        tail = new DLinkedNode();
-        tail.next = null;
-
         head.next = tail;
         tail.prev = head;
+        tail.next = null;
     }
 
-    private void addNode(DLinkedNode node) {
+    private void addNode(Node node) {
         //新增的節點會在最前面
         //設定node
         node.prev = head;
@@ -37,47 +38,44 @@ public class LRUCache {
         head.next = node;
     }
 
-    private void removeNode(DLinkedNode node) {
+    private void removeNode(Node node) {
         //紀錄前後指標
-        DLinkedNode pre = node.prev;
-        DLinkedNode post = node.next;
+        Node prev = node.prev;
+        Node next = node.next;
         //將指標跳過該node達到移除功能
-        pre.next = post;
-        post.prev = pre;
+        prev.next = next;
+        next.prev = prev;
     }
 
-    private void moveToHead(DLinkedNode node) {
+    private void moveNodeToHead(Node node) {
         //先移除他原本在的位子
         removeNode(node);
         //新增到head
         addNode(node);
     }
 
-    private DLinkedNode popTail() {
-        DLinkedNode tmp = tail.prev;
+    private Node popTail() {
+        Node tmp = tail.prev;
         removeNode(tmp);
         return tmp;
     }
 
     public int get(int key) {
-
-        DLinkedNode node = cache.get(key);
+        Node node = cache.get(key);
         if (node == null) {
             return -1;
         }
-
         //將該key移到頂點
-        this.moveToHead(node);
-
+        moveNodeToHead(node);
         return node.value;
     }
 
     public void put(int key, int value) {
-        DLinkedNode node = cache.get(key);
+        Node node = cache.get(key);
 
         if (node == null) {
             //生成新節點
-            DLinkedNode newNode = new DLinkedNode();
+            Node newNode = new Node();
             newNode.key = key;
             newNode.value = value;
 
@@ -87,22 +85,23 @@ public class LRUCache {
             currSize++;
             //超過容量則移除最後一個節點
             if (currSize > maxSize) {
-                DLinkedNode tail = popTail();
-                cache.remove(tail.key);
+                Node tmp = tail.prev;
+                removeNode(tmp);
+                cache.remove(tmp.key);
                 currSize--;
             }
         } else {
             //更新數值並移到頂點
             node.value = value;
-            moveToHead(node);
+            moveNodeToHead(node);
         }
     }
 
-    static class DLinkedNode {
+    static class Node {
         int key;
         int value;
-        DLinkedNode prev;
-        DLinkedNode next;
+        Node prev;
+        Node next;
     }
 
 }

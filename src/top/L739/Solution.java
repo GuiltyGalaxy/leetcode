89d181cbdab2ed79b8c39ee4e30a1dcbe1b29162
kllcoo1300 <kllcoo1300@gmail.com>
2023-08-03 02:31:30 +0800

    2023/08/02 [Top] 739. Daily Temperatures

--- src/top/L739/Solution.java
+++ src/top/L739/Solution.java
@@ -5,14 +5,44 @@ import java.util.Stack;
 class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
         Stack<Integer> stack = new Stack<>();
-        int[] ans = new int[temperatures.length];
-        for(int i = 0; i < temperatures.length; i++) {
-            while(!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]) {
+        int[] res = new int[temperatures.length];
+        for (int i = 0; i < temperatures.length; i++) {
+            // 遇到本日溫度大於stack的top
+            // 開始計算天數
+            while (!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]) {
+                // 計算過的天數移除
                 int idx = stack.pop();
-                ans[idx] = i - idx;
+                res[idx] = i - idx;
             }
             stack.push(i);
         }
-        return ans;
+        return res;
+    }
+
+    public int[] dailyTemperatures2(int[] temperatures) {
+        int n = temperatures.length;
+        int[] res = new int[n];
+        int max = Integer.MIN_VALUE;
+        // 從最後一天開始推算
+        for (int i = n - 1; i >= 0; i--) {
+            // 遇到比max還大做更新
+            if (temperatures[i] >= max) {
+                max = temperatures[i];
+                continue;
+            }
+            // 跑到這裡代表前幾天有一天溫度比今天大
+            // 先預設是前一天
+            int days = 1;
+            // 開始往前推算
+            while (temperatures[i + days] <= temperatures[i]) {
+                // 直接使用res來利用之前的結果推算差多少天
+                // 巧妙地避免掉要推算到實際天數的時間複雜度
+                // O(n)寫法為 days++;
+                // O(1)
+                days += res[i + days];
+            }
+            res[i] = days;
+        }
+        return res;
     }
 }
--- src/top/L739/SolutionTest.java
+++ src/top/L739/SolutionTest.java
@@ -10,11 +10,15 @@ class SolutionTest {
         Assertions.assertArrayEquals(
                 solution.dailyTemperatures(new int[]{73, 74, 75, 71, 69, 72, 76, 73}),
                 new int[]{1, 1, 4, 2, 1, 1, 0, 0});
+        Assertions.assertArrayEquals(
+                solution.dailyTemperatures2(new int[]{73, 74, 75, 71, 69, 72, 76, 73}),
+                new int[]{1, 1, 4, 2, 1, 1, 0, 0});
     }
 
     @Test
     public void case2() {
         Solution solution = new Solution();
         Assertions.assertArrayEquals(solution.dailyTemperatures(new int[]{30, 40, 50, 60}), new int[]{1, 1, 1, 0});
+        Assertions.assertArrayEquals(solution.dailyTemperatures2(new int[]{30, 40, 50, 60}), new int[]{1, 1, 1, 0});
     }
 }

8700fed68567a883628b0d1d0954639a13928f16
kllcoo1300 <kllcoo1300@gmail.com>
2023-08-04 01:22:42 +0800

    2023/08/04 [Top] 528. Random Pick with Weight

--- src/top/L528/Solution.java
+++ src/top/L528/Solution.java
@@ -4,31 +4,32 @@ import java.util.Random;
 
 class Solution {
 
-    Random r = new Random();
-    int[] wSum;
+    private final Random r = new Random();
+    private final int[] weight;
 
     public Solution(int[] w) {
-        //機率權重可以看成一個區段
+        // 機率權重可以看成一個區段
         // w = 1,3,6,10
         // wSum = 1,4,10,20
-        //在wSum中使用brainy search時的機率分布就會為w的分布
+        // 在wSum中使用brainy search時的機率分布就會為w的分布
         for (int i = 1; i < w.length; i++) {
             w[i] += w[i - 1];
         }
-        wSum = w;
+        weight = w;
     }
 
     public int pickIndex() {
 
-        int idx = r.nextInt(wSum[wSum.length - 1]) + 1;
+        int last = weight[weight.length - 1];
+        int idx = r.nextInt(last) + 1;
 
         int L = 0;
-        int R = wSum.length - 1;
+        int R = weight.length - 1;
         while (L < R) {
             int M = (L + R) / 2;
-            if (wSum[M] == idx) {
+            if (weight[M] == idx) {
                 return M;
-            } else if (wSum[M] > idx) {
+            } else if (weight[M] > idx) {
                 R = M;
             } else {
                 L = M + 1;

e88a9b85503d59277cb01dd19f07407c35257f82
kllcoo1300 <kllcoo1300@gmail.com>
2024-06-16 23:45:28 +0800

    2024/06/16 [Top] 322. Coin Change

--- src/top/L322/Solution.java
+++ src/top/L322/Solution.java
@@ -13,22 +13,22 @@ class Solution {
         // a:金額
         // b:最小換錢硬幣數
         int[] dp = new int[amount + 1];
+
         Arrays.fill(dp, amount + 1);
-        //0不用任何硬幣
+        // 0不用任何硬幣
         dp[0] = 0;
-        //從最小開始換
+        // 從最小開始換
         for (int coin : coins) {
-            //比較coin到amount換過一遍的結果
-            //起始為coin是因為你不可以用4去換dp[3]，只取一個就超過金額了
+            // 比較coin到amount換過一遍的結果
             for (int i = coin; i <= amount; i++) {
-                //取i - coin硬幣的數量
-                //+1代表說替換成該硬幣
-                //dp[i]的硬幣數是否會優於換成當前coin的數量
+                // 取i - coin硬幣的數量
+                // +1代表說替換成該硬幣
+                // dp[i]的硬幣數是否會優於換成當前coin的數量
                 dp[i] = Math.min(dp[i], 1 + dp[i - coin]);
             }
         }
 
-        //dp[amount] == amount + 1代表沒有任何DP走到結果
+        // dp[amount] == amount + 1代表沒有任何DP走到結果
         return dp[amount] == amount + 1 ? -1 : dp[amount];
 
     }
--- src/top/L322/SolutionTest.java
+++ src/top/L322/SolutionTest.java
@@ -7,6 +7,18 @@ class SolutionTest {
     @Test
     public void case1() {
         Solution solution = new Solution();
-        Assertions.assertEquals(solution.coinChange(new int[]{1, 2, 5}, 11), 3);
+        Assertions.assertEquals(3, solution.coinChange(new int[]{1, 2, 5}, 11));
+    }
+
+    @Test
+    public void case2() {
+        Solution solution = new Solution();
+        Assertions.assertEquals(-1, solution.coinChange(new int[]{2}, 3));
+    }
+
+    @Test
+    public void case3() {
+        Solution solution = new Solution();
+        Assertions.assertEquals(0, solution.coinChange(new int[]{1}, 0));
     }
 }

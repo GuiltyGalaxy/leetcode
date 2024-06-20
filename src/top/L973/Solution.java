a0beb6f3e3038f2f83df45a19526e1d8dcc26472
kllcoo1300 <kllcoo1300@gmail.com>
2024-06-21 00:41:40 +0800

    2024/06/20 [Top] 973. K Closest Points to Origin

--- src/top/L973/Solution.java
+++ src/top/L973/Solution.java
@@ -1,56 +1,94 @@
 package top.L973;
 
-import java.util.Arrays;
 import java.util.PriorityQueue;
+import java.util.Random;
 
 class Solution {
 
+    //平均O(N) quick select
+    private final Random rand = new Random();
+
     //O(Nlog(k))
     public int[][] kClosest(int[][] points, int k) {
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> {
-            int p1 = Math.abs(a[0]) * Math.abs(a[0]) + Math.abs(a[1]) * Math.abs(a[1]);
-            int p2 = Math.abs(b[0]) * Math.abs(b[0]) + Math.abs(b[1]) * Math.abs(b[1]);
-            return p1 - p2;
-        });
+        // 利用數學公式來計算兩點誰比較近
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) ->
+                (b[0] * b[0] + b[1] * b[1]) - (a[0] * a[0] + a[1] * a[1]));
 
-        pq.addAll(Arrays.asList(points));
+        for (int[] point : points) {
+            pq.add(point);
+            if (pq.size() > k) {
+                pq.poll();
+            }
+        }
 
+        // 提取對應的k
         int[][] ans = new int[k][2];
-        for (int i = 0; i < ans.length; i++) {
+        for (int i = 0; i < k; i++) {
             ans[i] = pq.poll();
         }
         return ans;
     }
 
+    public int[][] kClosest2(int[][] points, int K) {
+        quickSelect(points, 0, points.length - 1, K);
+        int[][] result = new int[K][2];
+        // 排序後取前K點就為解答
+        System.arraycopy(points, 0, result, 0, K);
+        return result;
+    }
 
-    //O(N) quick select用在這問題上剛好可以很快(非線性解)
-    public int[][] kClosest2(int[][] points, int k) {
-        int len = points.length, L = 0, R = len - 1;
-        while (L <= R) {
-            int M = helper(points, L, R);
-            if (M == k) break;
-            if (M < k) {
-                L = M + 1;
-            } else {
-                R = M - 1;
+    private void quickSelect(int[][] points, int left, int right, int K) {
+        // 左邊點的數量比右邊大就不計算了
+        if (left >= right) return;
+
+        // 隨機選擇一個點作為基準點
+        int pivotIndex = left + rand.nextInt(right - left + 1);
+        int pivotDist = dist(points[pivotIndex]);
+
+        // 將基準點移到最右邊
+        swap(points, pivotIndex, right);
+
+        int partitionIndex = partition(points, left, right, pivotDist);
+
+        // 如果partitionIndex - left + 1 == K代表滿足條件不繼續算
+        if (K < partitionIndex - left + 1) {
+            // 結果數量大於K繼續縮小範圍
+            quickSelect(points, left, partitionIndex - 1, K);
+        } else if (K > partitionIndex - left + 1) {
+            // 結果數量小於K繼續擴大範圍
+            quickSelect(points, partitionIndex + 1, right, K - (partitionIndex - left + 1));
+        }
+    }
+
+    /**
+     * 以pivotDist為基準區分points成left與right
+     * 最後結果points left到right的區間會被依照與pivotDist比較大小的結果分為左右兩邊
+     * 回傳partitionIndex來表示最終切分點位子
+     */
+    private int partition(int[][] points, int left, int right, int pivotDist) {
+        int l = left;
+        for (int r = left; r < right; r++) {
+            if (dist(points[r]) <= pivotDist) {
+                // 把該點移到左邊
+                swap(points, l, r);
+                l++;
             }
         }
-        return Arrays.copyOfRange(points, 0, k);
+        // 把最後的l位子與right交換達成分區
+        swap(points, l, right);
+        return l;
     }
 
-    private int helper(int[][] A, int L, int R) {
-        int[] pivot = A[L];
-        while (L < R) {
-            while (L < R && compare(A[R], pivot) >= 0) R--;
-            A[L] = A[R];
-            while (L < R && compare(A[L], pivot) <= 0) L++;
-            A[R] = A[L];
-        }
-        A[L] = pivot;
-        return L;
+    /**
+     * 計算該點與0的距離
+     */
+    private int dist(int[] point) {
+        return point[0] * point[0] + point[1] * point[1];
     }
 
-    private int compare(int[] p1, int[] p2) {
-        return p1[0] * p1[0] + p1[1] * p1[1] - p2[0] * p2[0] - p2[1] * p2[1];
+    private void swap(int[][] points, int i, int j) {
+        int[] temp = points[i];
+        points[i] = points[j];
+        points[j] = temp;
     }
 }
--- src/top/L973/SolutionTest.java
+++ src/top/L973/SolutionTest.java
@@ -7,12 +7,14 @@ class SolutionTest {
     @Test
     public void case1() {
         Solution solution = new Solution();
-        Assertions.assertArrayEquals(solution.kClosest(new int[][]{{1, 3}, {-2, 2}}, 1), new int[][]{{-2, 2}});
+        Assertions.assertArrayEquals(new int[][]{{-2, 2}}, solution.kClosest(new int[][]{{1, 3}, {-2, 2}}, 1));
+        Assertions.assertArrayEquals(new int[][]{{-2, 2}}, solution.kClosest2(new int[][]{{1, 3}, {-2, 2}}, 1));
     }
 
     @Test
     public void case2() {
         Solution solution = new Solution();
-        Assertions.assertArrayEquals(solution.kClosest(new int[][]{{3, 3}, {5, -1}, {-2, 4}}, 2), new int[][]{{3, 3}, {-2, 4}});
+        Assertions.assertArrayEquals(new int[][]{{-2, 4}, {3, 3}}, solution.kClosest(new int[][]{{3, 3}, {5, -1}, {-2, 4}}, 2));
+        Assertions.assertArrayEquals(new int[][]{{3, 3}, {-2, 4}}, solution.kClosest2(new int[][]{{3, 3}, {5, -1}, {-2, 4}}, 2));
     }
 }

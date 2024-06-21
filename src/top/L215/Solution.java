5b34f6ccf93b50cf765d8b585f3c58f7f52c0f8d
kllcoo1300 <kllcoo1300@gmail.com>
2024-06-22 02:14:37 +0800

    2024/06/21 [Top] 215. Kth Largest Element in an Array

--- src/top/L215/Solution.java
+++ src/top/L215/Solution.java
@@ -1,17 +1,77 @@
 package top.L215;
 
 
-import java.util.Arrays;
-import java.util.PriorityQueue;
+import java.util.Random;
 
 class Solution {
+    private static final Random random = new Random();
+
+    /**
+     * 平均O(n) 選擇排序
+     */
     public int findKthLargest(int[] nums, int k) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b - a);
-        Arrays.stream(nums).forEach(pq::add);
-        for (int i = 0; i < k - 1; i++) {
-            pq.poll();
+        return quickSelect(nums, 0, nums.length - 1, nums.length - k);
+    }
+
+    private int quickSelect(int[] nums, int left, int right, int k) {
+        if (left == right) {
+            return nums[left];
         }
-        assert !pq.isEmpty();
-        return pq.peek();
+
+        int pivotIndex = left + random.nextInt(right - left + 1);
+        pivotIndex = partition(nums, left, right, pivotIndex);
+
+        if (k == pivotIndex) {
+            return nums[k];
+        } else if (k < pivotIndex) {
+            return quickSelect(nums, left, pivotIndex - 1, k);
+        } else {
+            return quickSelect(nums, pivotIndex + 1, right, k);
+        }
+    }
+
+    private int partition(int[] nums, int left, int right, int pivotIndex) {
+        int pivotValue = nums[pivotIndex];
+        swap(nums, pivotIndex, right);
+        int storeIndex = left;
+
+        for (int i = left; i < right; i++) {
+            if (nums[i] < pivotValue) {
+                swap(nums, storeIndex, i);
+                storeIndex++;
+            }
+        }
+
+        swap(nums, storeIndex, right);
+        return storeIndex;
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+
+    /**
+     * 取巧方式
+     */
+    public int findKthLargest2(int[] nums, int k) {
+        // 由於題目有數字範圍，所以建立一個array來存放所有數字出現次數
+        int[] counter = new int[20_001];
+        // 統計出現次數
+        for (int num : nums) {
+            counter[num + 10_000]++;
+        }
+
+        int cnt = 0;
+        // 從最小的值開始計算出現次數，當大於k就是解答了
+        for (int i = counter.length - 1; i >= 0; i--) {
+            cnt += counter[i];
+            if (cnt >= k) {
+                return i - 10_000;
+            }
+        }
+        return -1;
     }
 }
--- src/top/L215/SolutionTest.java
+++ src/top/L215/SolutionTest.java
@@ -7,12 +7,14 @@ class SolutionTest {
     @Test
     public void case1() {
         Solution solution = new Solution();
-        Assertions.assertEquals(solution.findKthLargest(new int[]{3, 2, 1, 5, 6, 4}, 2), 5);
+        Assertions.assertEquals(5, solution.findKthLargest(new int[]{3, 2, 1, 5, 6, 4}, 2));
+        Assertions.assertEquals(5, solution.findKthLargest2(new int[]{3, 2, 1, 5, 6, 4}, 2));
     }
 
     @Test
     public void case2() {
         Solution solution = new Solution();
-        Assertions.assertEquals(solution.findKthLargest(new int[]{3, 2, 3, 1, 2, 4, 5, 5, 6}, 4), 4);
+        Assertions.assertEquals(4, solution.findKthLargest(new int[]{3, 2, 3, 1, 2, 4, 5, 5, 6}, 4));
+        Assertions.assertEquals(4, solution.findKthLargest2(new int[]{3, 2, 3, 1, 2, 4, 5, 5, 6}, 4));
     }
 }

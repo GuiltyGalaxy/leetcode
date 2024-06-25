45846e04834d5a752b7684e54b1f6aa81f300550
kllcoo1300 <kllcoo1300@gmail.com>
2024-06-25 23:56:40 +0800

    2024/06/25 [Top] 39. Combination Sum

--- src/top/L39/Solution.java
+++ src/top/L39/Solution.java
@@ -14,15 +14,19 @@ class Solution {
     }
 
     private void helper(int[] candidates, int target, int index, List<Integer> current) {
+        // 當target為0代表current中為一組解答
         if (target == 0) {
             ans.add(new ArrayList<>(current));
             return;
         }
 
         for (int i = index; i < candidates.length; i++) {
+            // candidates[i]就比target大的話不用繼續處理
             if (candidates[i] > target) continue;
+            // 加入該值繼續算
             current.add(candidates[i]);
             helper(candidates, target - candidates[i], i, current);
+            // 移除該值(一定會在最後所以移除最後就好)
             current.remove(current.size() - 1);
         }
 

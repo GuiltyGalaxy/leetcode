6d877071f7e82bb510fd643704c1cdb32cebc95f
kllcoo1300 <kllcoo1300@gmail.com>
2024-06-25 23:01:57 +0800

    2024/06/25 [Top] 57. Insert Interval

--- src/top/L57/Solution.java
+++ src/top/L57/Solution.java
@@ -1,44 +1,34 @@
 package top.L57;
 
-import java.util.LinkedList;
+import java.util.ArrayList;
+import java.util.List;
 
 class Solution {
 
     public int[][] insert(int[][] intervals, int[] newInterval) {
+        List<int[]> result = new ArrayList<>();
 
-        LinkedList<int[]> ans = new LinkedList<>();
-        int idx = 0;
-
-        //尋找起點
-        while (idx < intervals.length) {
-            if (intervals[idx][0] < newInterval[0]) {
-                ans.add(intervals[idx]);
-                idx++;
-            } else {
-                //找到比newInterval[0]大的就停住
-                break;
-            }
+        int i = 0;
+        // 由於小於newInterval[0]的區間一定不可能被合併，所以直接不處理
+        while (i < intervals.length && intervals[i][1] < newInterval[0]) {
+            result.add(intervals[i]);
+            i++;
         }
 
-        if (ans.size() == 0 || (newInterval[0] > ans.getLast()[1])) {
-            //代表newInterval完全沒有overlapping於intervals，且為最後面
-            ans.add(newInterval);
-        } else {
-            //取最後一組比較[1]的大小替換
-            int[] lastInterval = ans.getLast();
-            lastInterval[1] = Math.max(lastInterval[1], newInterval[1]);
+        // 合併與newInterval重疊的區間
+        while (i < intervals.length && intervals[i][0] <= newInterval[1]) {
+            newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
+            newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
+            i++;
+        }
+        result.add(newInterval);
+
+        // 處理剩下的區間
+        while (i < intervals.length) {
+            result.add(intervals[i]);
+            i++;
         }
 
-        while (idx < intervals.length) {
-            int[] lastInterval = ans.getLast();
-            //不斷比較lastInterval直到下一組的intervals[0]大於lastInterval[1]
-            if (lastInterval[1] >= intervals[idx][0]) {
-                lastInterval[1] = Math.max(lastInterval[1], intervals[idx][1]);
-            } else {
-                ans.add(intervals[idx]);
-            }
-            idx++;
-        }
-        return ans.toArray(new int[ans.size()][]);
+        return result.toArray(new int[result.size()][]);
     }
 }
--- src/top/L57/SolutionTest.java
+++ src/top/L57/SolutionTest.java
@@ -7,14 +7,12 @@ class SolutionTest {
     @Test
     public void case1() {
         Solution solution = new Solution();
-        Assertions.assertArrayEquals(solution.insert(new int[][]{{1, 3}, {6, 9}}, new int[]{2, 5}),
-                new int[][]{{1, 5}, {6, 9}});
+        Assertions.assertArrayEquals(new int[][]{{1, 5}, {6, 9}}, solution.insert(new int[][]{{1, 3}, {6, 9}}, new int[]{2, 5}));
     }
 
     @Test
     public void case2() {
         Solution solution = new Solution();
-        Assertions.assertArrayEquals(solution.insert(new int[][]{{1, 2}, {3, 5}, {6, 7}, {8, 10}, {12, 16}}, new int[]{4, 8}),
-                new int[][]{{1, 2}, {3, 10}, {12, 16}});
+        Assertions.assertArrayEquals(new int[][]{{1, 2}, {3, 10}, {12, 16}}, solution.insert(new int[][]{{1, 2}, {3, 5}, {6, 7}, {8, 10}, {12, 16}}, new int[]{4, 8}));
     }
 }

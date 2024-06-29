c33bbabae40042041f146d64fdfbee38820aca40
kllcoo1300 <kllcoo1300@gmail.com>
2024-06-29 22:34:01 +0800

    2024/06/29 [Top] 239. Sliding Window Maximum

--- src/top/L239/Solution.java
+++ src/top/L239/Solution.java
@@ -1,8 +1,12 @@
 package top.L239;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 class Solution {
 
     /**
+     * 跟Deque都是O(n) 但少了維護Deque的操作實際上快了一些
      * 計算原理
      * 假設arr[8] = 1,3,-1,-3,5,3,6,7
      * k = 5
@@ -24,28 +28,28 @@ class Solution {
      * 每個window最大值會為
      * Math.max(L->R[x+k],R->L[x])
      */
-    public int[] maxSlidingWindow(int[] arr, int k) {
-        int[] ret = new int[arr.length - k + 1];
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int[] ret = new int[nums.length - k + 1];
         // 紀錄兩種計算方向的MAX為多少
-        int[] leftDirMax = new int[arr.length];
-        int[] rightDirMax = new int[arr.length];
+        int[] leftDirMax = new int[nums.length];
+        int[] rightDirMax = new int[nums.length];
         // window一次位移k
-        for (int i = 0; i < arr.length; i += k) {
+        for (int i = 0; i < nums.length; i += k) {
             // 當前window右邊邊界
-            int rightBound = Math.min(i + k - 1, arr.length - 1);
+            int rightBound = Math.min(i + k - 1, nums.length - 1);
             int max = Integer.MIN_VALUE;
             // 從左邊開始往右的最大值紀錄
             for (int j = i; j <= rightBound; j++) {
-                if (arr[j] > max) {
-                    max = arr[j];
+                if (nums[j] > max) {
+                    max = nums[j];
                 }
                 rightDirMax[j] = max;
             }
             // 從右邊開始往左的最大值紀錄
             max = Integer.MIN_VALUE;
             for (int j = rightBound; j >= i; j--) {
-                if (arr[j] > max) {
-                    max = arr[j];
+                if (nums[j] > max) {
+                    max = nums[j];
                 }
                 leftDirMax[j] = max;
             }
@@ -58,5 +62,37 @@ class Solution {
             ret[i] = Math.max(rightDirMax[end], leftDirMax[i]);
         }
         return ret;
+    }
+
+    /**
+     * Deque O(n)
+     */
+    public int[] maxSlidingWindow2(int[] nums, int k) {
+        int n = nums.length;
+        int[] result = new int[n - k + 1];
+        // 維護一個ArrayDeque,其中第一個index一定是目前窗口中最大值
+        Deque<Integer> deque = new ArrayDeque<>();
+
+        for (int i = 0; i < n; i++) {
+            // 移出超出窗口的index
+            if (!deque.isEmpty() && deque.peekFirst() < i - k + 1) {
+                deque.pollFirst();
+            }
+
+            // 將比當前數字小的index從尾端依序移除
+            while (!deque.isEmpty() && nums[deque.peekLast()] < nums[i]) {
+                deque.pollLast();
+            }
+
+            // 將當前index加入
+            deque.offerLast(i);
+
+            // 如果result尚未填滿則將該次結果填入
+            if (i >= k - 1) {
+                result[i - k + 1] = nums[deque.peekFirst()];
+            }
+        }
+
+        return result;
     }
 }
--- src/top/L239/SolutionTest.java
+++ src/top/L239/SolutionTest.java
@@ -7,9 +7,10 @@ class SolutionTest {
     @Test
     public void case1() {
         Solution solution = new Solution();
-        Assertions.assertArrayEquals(solution.maxSlidingWindow(
-                new int[]{1, 3, -1, -3, 5, 3, 6, 7}, 3),
-                new int[]{3, 3, 5, 5, 6, 7});
+        Assertions.assertArrayEquals(new int[]{3, 3, 5, 5, 6, 7},
+                solution.maxSlidingWindow(new int[]{1, 3, -1, -3, 5, 3, 6, 7}, 3));
+        Assertions.assertArrayEquals(new int[]{3, 3, 5, 5, 6, 7},
+                solution.maxSlidingWindow2(new int[]{1, 3, -1, -3, 5, 3, 6, 7}, 3));
     }
 
 }

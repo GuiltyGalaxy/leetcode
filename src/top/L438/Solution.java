db2d2d111bf9b7415c45ae3945cac7d7b0389e41
Galaxy <kllcoo1300@gmail.com>
2024-07-05 02:40:47 +0800

    2024/07/04 [Top] 438. Find All Anagrams in a String

--- src/top/L438/Solution.java
+++ src/top/L438/Solution.java
@@ -6,29 +6,35 @@ import java.util.List;
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
         List<Integer> ans = new ArrayList<>();
+
+        char[] sA = s.toCharArray();
+        char[] sP = p.toCharArray();
         int sLen = s.length();
         int pLen = p.length();
+
+        // p比s長的話不可能有答案
         if (pLen > sLen) {
             return ans;
         }
-        //計算char數
-        //並且是以p的長度當window
+        // 計算char數
         int[] sMap = new int[26];
         int[] pMap = new int[26];
+        // 以p的長度當起始window
         for (int i = 0; i < pLen; i++) {
-            sMap[s.charAt(i) - 'a']++;
-            pMap[p.charAt(i) - 'a']++;
+            sMap[sA[i] - 'a']++;
+            pMap[sP[i] - 'a']++;
         }
 
         for (int i = 0; i <= sLen - pLen; i++) {
             if (isMapping(sMap, pMap)) {
                 ans.add(i);
             }
-            //整體後移
-            sMap[s.charAt(i) - 'a']--;
+            // window向後移動
+            // 把頭元素去掉
+            sMap[sA[i] - 'a']--;
+            // 尚未超出sLen邊界的情況下，尾端新增元素
             if (i + pLen < sLen) {
-                //長度夠的情況下要連同後面一起加
-                sMap[s.charAt(i + pLen) - 'a']++;
+                sMap[sA[i + pLen] - 'a']++;
             }
         }
         return ans;

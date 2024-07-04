c0e39232ca4d6c1327a258bf1d2e58db5e727871
Galaxy <kllcoo1300@gmail.com>
2024-07-05 02:12:37 +0800

    2024/07/04 [Top] 5. Longest Palindromic Substring

--- src/top/L5/Solution.java
+++ src/top/L5/Solution.java
@@ -10,9 +10,8 @@ public class Solution {
     public String longestPalindrome(String s) {
         char[] str = s.toCharArray();
         int n = str.length;
-        // n / 2 => n >> 1
         // 從中間開始找
-        expandCenter(str, n >> 1, MID);
+        expandCenter(str, n / 2, MID);
         return new String(str, begin, maxLen);
     }
 
@@ -22,14 +21,13 @@ public class Solution {
         int j = idx + 1;
 
         // 撇除掉起始相同的字
-        // ex. "naaaaaaan"
-        //      ^   ^   ^
-        //      |   |   |
-        //      i  idx  j
-        while (i >= 0 && str[i] == str[idx])
-            i--;
-        while (j < len && str[j] == str[idx])
-            j++;
+        // 這動作是為了避免偶數的情況 "abba"
+        // ex. "naaaaaan"
+        //      ^   ^  ^
+        //      |   |  |
+        //      i  idx j
+        while (i >= 0 && str[i] == str[idx]) i--;
+        while (j < len && str[j] == str[idx]) j++;
 
         // 計算最大回文長度
         int left = i;
@@ -45,12 +43,15 @@ public class Solution {
             maxLen = right - left - 1;
         }
 
-        // 剩下的長度如果比目前maxLen大再繼續往左右找
-        if (((i + 1) << 1) > maxLen && direction <= MID)
+        // 剩下的長度如果比目前maxLen大
+        // 代表還有更長的可能，所以繼續找
+        if (((i + 1) * 2) > maxLen && direction <= MID) {
             expandCenter(str, i, LEFT);
+        }
 
-        if (((len - j) << 1) > maxLen && direction >= MID)
+        if (((len - j) * 2) > maxLen && direction >= MID) {
             expandCenter(str, j, RIGHT);
+        }
     }
 
 }

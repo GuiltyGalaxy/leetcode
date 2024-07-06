04a0c15287ac15ef63e2d261107866e140e8cc55
Galaxy <kllcoo1300@gmail.com>
2024-07-06 22:04:41 +0800

    2024/07/06 [Top] 14. Longest Common Prefix

--- src/top/L14/Solution.java
+++ src/top/L14/Solution.java
@@ -2,34 +2,23 @@ package top.L14;
 
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-
-        String minWord = strs[0];
-        //找出長度最小的字串
-        for (String s : strs) {
-            if (s.length() < minWord.length()) {
-                minWord = s;
-            }
-        }
-
-        if (minWord.length() == 0) return "";
-
-        while (minWord.length() != 0) {
-            int counter = 0;
-            //尋找符合的目標數
-            for (String s : strs) {
-                if (s.startsWith(minWord)) {
-                    counter++;
+        // 由於取全部字串的最小相同前綴，所以取哪個先當基準都沒差
+        // 假設第一個為解答
+        String ans = strs[0];
+        int len = ans.length();
+        for (int i = 1; i < strs.length; i++) {
+            String s = strs[i];
+            // 只要與當前s不符合則減去最後一個字
+            // 或當前長度大於目前的s
+            while (len > s.length() || !ans.equals(s.substring(0, len))) {
+                len--;
+                // 0代表沒有任何答案
+                if (len == 0) {
+                    return "";
                 }
-            }
-            if (counter == strs.length) {
-                //符合array所有string
-                return minWord;
-            } else {
-                //移除最後一個字
-                minWord = minWord.substring(0, minWord.length() - 1);
+                ans = ans.substring(0, len);
             }
         }
-
-        return minWord;
+        return ans;
     }
 }

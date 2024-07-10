2681fa2898238b80fd7f249c1f9e33a22fc1ddb6
Galaxy <kllcoo1300@gmail.com>
2024-07-11 00:00:35 +0800

    2024/07/10 [Top] 1235. Maximum Profit in Job Scheduling

--- src/top/L1235/Solution.java
+++ src/top/L1235/Solution.java
@@ -1,49 +1,73 @@
 package top.L1235;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 class Solution {
     public int jobScheduling(int[] startTime, int[] endTime, int[] profit) {
-        int n = startTime.length;
-        int[][] map = new int[n][3];
 
-        //以endTime排序
-        for (int i = 0; i < n; i++) {
-            map[i][0] = startTime[i];
-            map[i][1] = endTime[i];
-            map[i][2] = profit[i];
+        int len = profit.length;
+        // 建立Job
+        Job[] jobs = new Job[len];
+        for (int i = 0; i < len; i++) {
+            jobs[i] = new Job(startTime[i], endTime[i], profit[i]);
         }
 
-        Arrays.sort(map, (a, b) -> a[1] - b[1]);
+        // 按照結束時間排序
+        Arrays.sort(jobs, Comparator.comparingInt(a -> a.end));
 
-        int[] maxProfit = new int[n];
-        maxProfit[0] = map[0][2];
+        // dp紀錄job工作最大效益
+        // index紀錄選擇工作的次數(最大情況就是所有工作都不衝突，全選)
+        int[] dp = new int[len];
+        // 先填入第一個最早結束的工作
+        dp[0] = jobs[0].profit;
 
-        for (int i = 1; i < n; i++) {
-            int prvIndex = i - 1;
-            int start = map[i][0];
-            int earn = map[i][2];
+        for (int i = 1; i < len; i++) {
 
-            if (map[prvIndex][1] > start) {
-                //prv end有重疊到當前的start
-                //prvIndex往前移到end < start為止
-                while (prvIndex >= 0 && map[prvIndex][1] > start) {
-                    prvIndex--;
-                }
+            int left = 0;
+            int right = i - 1;
+            // 本次所找到最佳目標
+            int res = -1;
 
-                if (prvIndex >= 0) {
-                    //有找到前一個不重疊的區域，則代表profit是可以疊加前一個的
-                    maxProfit[i] = Math.max(maxProfit[prvIndex] + earn, maxProfit[i - 1]);
+            // 由於是利用結束時間排序
+            // 所以可以使用binary search去尋最Job中結束時間最接近本次起始時間的
+            // 就可以於本次工作前加入其他工作
+            while (left <= right) {
+                int mid = left + ((right - left) >> 1);
+                if (jobs[mid].end <= jobs[i].start) {
+                    res = mid;
+                    left = mid + 1;
                 } else {
-                    //沒有的情況下(prvIndex==-1)，就只能單獨比profit
-                    maxProfit[i] = Math.max(earn, maxProfit[i - 1]);
+                    right = mid - 1;
                 }
-            } else {
-                //沒重疊直接將profit加上
-                maxProfit[i] = maxProfit[prvIndex] + earn;
             }
+
+            // 做本次job所獲的效益
+            int doCurJob = jobs[i].profit;
+            // 如果有找到匹配的則把匹配到的dp結果也加入
+            if (res != -1) {
+                doCurJob += dp[res];
+            }
+
+            // 不做本次job的效益
+            int notDoCurJob = dp[i - 1];
+
+            // 比較有效益的加入dp
+            dp[i] = Math.max(doCurJob, notDoCurJob);
         }
 
-        return maxProfit[n - 1];
+        return dp[len - 1];
+    }
+
+    static class Job {
+        int start;
+        int end;
+        int profit;
+
+        public Job(int start, int end, int profit) {
+            this.start = start;
+            this.end = end;
+            this.profit = profit;
+        }
     }
 }
--- src/top/L1235/SolutionTest.java
+++ src/top/L1235/SolutionTest.java
@@ -7,13 +7,13 @@ class SolutionTest {
     @Test
     public void case1() {
         Solution solution = new Solution();
-        Assertions.assertEquals(solution.jobScheduling(new int[]{1, 2, 3, 3}, new int[]{3, 4, 5, 6}, new int[]{50, 10, 40, 70}), 120);
+        Assertions.assertEquals(120, solution.jobScheduling(new int[]{1, 2, 3, 3}, new int[]{3, 4, 5, 6}, new int[]{50, 10, 40, 70}));
     }
 
     @Test
     public void case2() {
         Solution solution = new Solution();
-        Assertions.assertEquals(solution.jobScheduling(new int[]{1, 2, 3, 4, 6}, new int[]{3, 5, 10, 6, 9}, new int[]{20, 20, 100, 70, 60}), 150);
+        Assertions.assertEquals(150, solution.jobScheduling(new int[]{1, 2, 3, 4, 6}, new int[]{3, 5, 10, 6, 9}, new int[]{20, 20, 100, 70, 60}));
     }
 
 }

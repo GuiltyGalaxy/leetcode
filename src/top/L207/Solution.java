47655d9d3a59de8a51148111f2586728eabd21bb
Galaxy <kllcoo1300@gmail.com>
2024-07-13 21:15:44 +0800

    2024/07/13 [Top] 207. Course Schedule

--- src/top/L207/Solution.java
+++ src/top/L207/Solution.java
@@ -5,86 +5,93 @@ import java.util.List;
 import java.util.Queue;
 
 class Solution {
+    /**
+     * 檢測是否為拓譜排序
+     */
     public boolean canFinish(int numCourses, int[][] prerequisites) {
 
-        //進入點次數
-        int[] incomingEdges = new int[numCourses];
-        //方向紀錄
-        List<Integer>[] goCourses = new List[numCourses];
-        for (int i = 0; i < goCourses.length; i++) {
-            goCourses[i] = new LinkedList<>();
+        // 紀錄每個課前面有多少課
+        int[] inCourses = new int[numCourses];
+        // 紀錄每個課下面有多少課
+        List<Integer>[] outCourses = new List[numCourses];
+        for (int i = 0; i < outCourses.length; i++) {
+            outCourses[i] = new LinkedList<>();
         }
-        //建立拓譜排序
-        //依賴關系為
-        //[1,0] => 0->1
+        // 建立拓譜排序
+        // 依賴關系為
+        // [1,0] => 0->1
         for (int[] pair : prerequisites) {
-            incomingEdges[pair[0]]++;
-            //List是為了可記錄多點方向
-            //[1,0],[2,0] => 0->1
-            //                └>2
-            goCourses[pair[1]].add(pair[0]);
+            inCourses[pair[0]]++;
+            // List是為了可記錄多點方向
+            // [1,0],[2,0] => 0->1
+            //                 └>2
+            outCourses[pair[1]].add(pair[0]);
         }
 
-        //從incomingEdges==0開始做移除
+        // 從前面沒有課的課開始計算
         Queue<Integer> queue = new LinkedList<>();
-        for (int i = 0; i < incomingEdges.length; i++) {
-            if (incomingEdges[i] == 0) {
+        for (int i = 0; i < inCourses.length; i++) {
+            if (inCourses[i] == 0) {
                 queue.add(i);
             }
         }
-        //edgeCnt紀錄所移除的邊
+        // 記錄所有的邊有多少
         int edgeCnt = prerequisites.length;
         while (!queue.isEmpty()) {
             int cur = queue.poll();
-            //將該點所有的邊移除
-            for (int goCrs : goCourses[cur]) {
+            // 移除該課程
+            // 並更新相關紀錄
+            for (int nextCourse : outCourses[cur]) {
                 edgeCnt--;
-                //移除的過程中如果該點也變頂點
-                //則加入下一個移除循環
-                if (--incomingEdges[goCrs] == 0) {
-                    queue.add(goCrs);
+                // 移除的過程中如果該課也變起始
+                // 則加入下一個計算循環
+                if (--inCourses[nextCourse] == 0) {
+                    queue.add(nextCourse);
                 }
             }
         }
-        //邊都可以完全移除代表拓譜排序成立
+        // 邊都可以完全移除代表拓譜排序成立
         return edgeCnt == 0;
     }
 
+    /**
+     * 利用紀錄課程的狀態解題
+     */
     public boolean canFinish2(int numCourses, int[][] prerequisites) {
 
         int n = prerequisites.length;
-        //紀錄已上過的課
-        boolean[] isLearn = new boolean[n];
-        int[] courses = new int[numCourses];
+        // 紀錄每個課出現幾次
+        int[] coursesCnt = new int[numCourses];
+        // 紀錄該關聯是否已被計算過
+        boolean[] isDone = new boolean[n];
 
-        //計算目標課程數
-        //應有可能出現[0,3] [1,3]
-        //代表修3要修過0,1
-        for (int[] prerequisite : prerequisites) {
-            courses[prerequisite[1]]++;
+        // 先把每個前置課都記錄下來
+        for (int[] c : prerequisites) {
+            coursesCnt[c[1]]++;
         }
 
-        boolean flag = true;
-
-        while (flag) {
-            flag = false;
-
+        boolean needContinue = true;
+        while (needContinue) {
+            needContinue = false;
             for (int i = 0; i < n; i++) {
-                if (!isLearn[i]) {
-                    //當前要修的課前修堂數==0代表該課程可以學習
-                    if (courses[prerequisites[i][0]] == 0) {
-                        //將對應下一堂目標課所需前修堂數-1
-                        courses[prerequisites[i][1]]--;
-                        isLearn[i] = true;
-                        flag = true;
-                    }
+                // 該關聯如果尚未完成
+                // 且課程也尚未上過
+                // 則更新該課對應前置課程並繼續計算
+                if (!isDone[i] && coursesCnt[prerequisites[i][0]] == 0) {
+                    isDone[i] = true;
+                    coursesCnt[prerequisites[i][1]]--;
+                    // 本次計算還有找到isDone的則要繼續往下計算
+                    needContinue = true;
                 }
             }
         }
 
-        //有任何課程有剩餘未修代表沒辦法全部修完
-        for (int i : courses) {
-            if (i != 0) return false;
+        // 檢查是否所有課程計數都為0
+        // 拓譜排序成立的情況下會達成該條件
+        for (int i = 0; i < numCourses; i++) {
+            if (coursesCnt[i] != 0) {
+                return false;
+            }
         }
 
         return true;

641d7369551538cc78fc7510ac9a30c0041f8574
Galaxy <kllcoo1300@gmail.com>
2024-07-15 21:59:22 +0800

    2024/07/15 [Top] 127. Word Ladder

--- src/top/L127/Solution.java
+++ src/top/L127/Solution.java
@@ -8,59 +8,60 @@ class Solution {
 
         HashSet<String> start = new HashSet<>();
         HashSet<String> end = new HashSet<>();
-        HashSet<String> dic = new HashSet<>(wordList);
+        HashSet<String> dict = new HashSet<>(wordList);
 
         start.add(beginWord);
         end.add(endWord);
 
-        if (!dic.contains(endWord)) {
+        // endWord不再字典中則無解
+        if (!dict.contains(endWord)) {
             return 0;
         }
-        //已使用beginWord、beginWord，所以從2開始
-        return bfs(start, end, dic, 2);
+        // 已使用beginWord、beginWord，所以從2開始
+        return bfs(start, end, dict, 2);
     }
 
-    public int bfs(HashSet<String> start, HashSet<String> end, HashSet<String> dic, int step) {
-        //沒有st了代表無結果
-        if (start.size() == 0) {
+    public int bfs(HashSet<String> start, HashSet<String> end, HashSet<String> dict, int step) {
+        // 無start代表無結果(next沒有找到任何可以往下計算的)
+        if (start.isEmpty()) {
             return 0;
         }
 
         if (start.size() > end.size()) {
-            //start和end挑少的來做，不一定都要以start為起始
-            return bfs(end, start, dic, step);
+            // start和end輪流計算，以平衡計算量，避免每次答案都在最後一個
+            return bfs(end, start, dict, step);
         }
-        //移除dic中的start，確保下次不會再拿到
-        dic.removeAll(start);
-        //開始紀錄下一輪符合的個數
+        // 使用start作計算，所以移除dict中的start
+        dict.removeAll(start);
+        // 開始紀錄下一輪符合的個數
         HashSet<String> next = new HashSet<>();
         for (String s : start) {
             char[] arr = s.toCharArray();
-            //逐個替換其中一個字元
+            // 逐個替換其中一個字元
             for (int i = 0; i < arr.length; i++) {
                 char tmp = arr[i];
-                //a-z都跑一遍
+                // a-z都跑一遍
                 for (char c = 'a'; c <= 'z'; c++) {
                     if (tmp == c) {
                         continue;
                     }
                     arr[i] = c;
-                    //檢查該差異的情況是否有符合在dic中
-                    String nstr = new String(arr);
-                    if (dic.contains(nstr)) {
-                        if (end.contains(nstr)) {
-                            //找到答案
+                    // 檢查該差異的情況是否有符合在dic中
+                    String tmpString = new String(arr);
+                    if (dict.contains(tmpString)) {
+                        if (end.contains(tmpString)) {
+                            // 找到答案
                             return step;
                         } else {
-                            next.add(nstr);
+                            next.add(tmpString);
                         }
                     }
                 }
-                //復原arr
+                // 復原arr
                 arr[i] = tmp;
             }
         }
-        //往下繼續找
-        return bfs(next, end, dic, step + 1);
+        // 往下繼續找
+        return bfs(next, end, dict, step + 1);
     }
 }

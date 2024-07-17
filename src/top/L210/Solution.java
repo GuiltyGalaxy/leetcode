3e5d45abaa4a01e15cdfc1eef56d1b7dadbc4d51
Galaxy <kllcoo1300@gmail.com>
2024-07-17 23:54:03 +0800

    2024/07/17 [Top] 210. Course Schedule II

--- src/top/L210/Solution.java
+++ src/top/L210/Solution.java
@@ -9,9 +9,9 @@ public class Solution {
         int[] res = new int[numCourses];
 
         // 建立拓譜圖
-        ArrayList[] adjList = new ArrayList[numCourses];
+        ArrayList<Integer>[] adjList = new ArrayList[numCourses];
         for (int i = 0; i < numCourses; i++) {
-            adjList[i] = new ArrayList<Integer>();
+            adjList[i] = new ArrayList<>();
         }
         for (int[] pair : prerequisites) {
             adjList[pair[0]].add(pair[1]);
@@ -24,7 +24,7 @@ public class Solution {
         int[] visited = new int[numCourses];
         for (int i = 0; i < numCourses; i++) {
             // DFS找出可修完所有課的順序
-            if (dfs(i, visited, adjList, res)) {
+            if (!dfs(i, visited, adjList, res)) {
                 return new int[0];
             }
         }
@@ -33,7 +33,7 @@ public class Solution {
 
     private boolean dfs(int i, int[] visited, ArrayList<Integer>[] adjList, int[] res) {
         if (visited[i] == 2) {
-            return false;
+            return true;
         }
         //要修該課前的課是否都修過了
         for (int req : adjList[i]) {
@@ -42,17 +42,17 @@ public class Solution {
                 // 先暫時改成查找中
                 visited[i] = 1;
                 // 以該點為主DFS
-                if (dfs(req, visited, adjList, res)) {
-                    return true;
+                if (!dfs(req, visited, adjList, res)) {
+                    return false;
                 }
             } else if (visited[req] == 1) {
                 // 遇到查找中的代表該修課順序不可能
-                return true;
+                return false;
             }
         }
         // 修完該課並記錄於答案中
         visited[i] = 2;
         res[idx++] = i;
-        return false;
+        return true;
     }
 }

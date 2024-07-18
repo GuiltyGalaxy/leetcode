f8cc96b0e23db68e6c7efcc5c233be138e8bf6c6
Galaxy <kllcoo1300@gmail.com>
2024-07-19 05:44:40 +0800

    2024/07/18 [Top] 329. Longest Increasing Path in a Matrix

--- src/top/L329/Solution.java
+++ src/top/L329/Solution.java
@@ -22,42 +22,27 @@ class Solution {
     private int dfs(int[][] matrix, int[][] max, int i, int j) {
 
         int res = 1;
-        // 主要四邊都去走
-        // 如果周邊有曾經走過的結果則依該結果判斷大小
-        // 比較大就+1當成該格基準點
-        // 第一次走的方向為基準所以就不用加上max判斷
-        // 上
-        if (i - 1 >= 0 && matrix[i - 1][j] > matrix[i][j]) {
-            if (max[i - 1][j] > 0) {
-                res = max[i - 1][j] + 1;
+        // 去檢查4個方向的max，來計算該格的max是多少
+        int[][] dirt = new int[][]{{0, -1}, {0, 1}, {1, 0}, {-1, 0}};
+        for (int[] d : dirt) {
+            int row = i + d[0];
+            int col = j + d[1];
+            if (row < 0 || row == matrix.length || col < 0 || col == matrix[0].length) {
+                continue;
+            }
+            // 前一格要大於當前才計算
+            if (matrix[row][col] <= matrix[i][j]) {
+                continue;
+            }
+            if (max[row][col] > 0) {
+                // 上一格被走過直接用max計算
+                res = Math.max(res, max[row][col] + 1);
             } else {
-                res = dfs(matrix, max, i - 1, j) + 1;
+                // 沒計算過則以該點為基準繼續往下算
+                res = Math.max(res, dfs(matrix, max, row, col) + 1);
             }
         }
-        // 左
-        if (j - 1 >= 0 && matrix[i][j - 1] > matrix[i][j]) {
-            if (max[i][j - 1] > 0) {
-                res = Math.max(max[i][j - 1] + 1, res);
-            } else {
-                res = Math.max(res, dfs(matrix, max, i, j - 1) + 1);
-            }
-        }
-        // 下
-        if (i + 1 < matrix.length && matrix[i + 1][j] > matrix[i][j]) {
-            if (max[i + 1][j] > 0) {
-                res = Math.max(max[i + 1][j] + 1, res);
-            } else {
-                res = Math.max(dfs(matrix, max, i + 1, j) + 1, res);
-            }
-        }
-        // 右
-        if (j + 1 < matrix[0].length && matrix[i][j + 1] > matrix[i][j]) {
-            if (max[i][j + 1] > 0) {
-                res = Math.max(max[i][j + 1] + 1, res);
-            } else {
-                res = Math.max(dfs(matrix, max, i, j + 1) + 1, res);
-            }
-        }
+        // 被最後結果更新到該點max上
         max[i][j] = res;
 
         return res;

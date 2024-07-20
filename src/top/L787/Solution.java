d23c60a4d4c0a741784377e3dfb0206b5d60881c
Galaxy <kllcoo1300@gmail.com>
2024-07-20 22:26:06 +0800

    2024/07/20 [Top] 787. Cheapest Flights Within K Stops

--- src/top/L787/Solution.java
+++ src/top/L787/Solution.java
@@ -4,14 +4,16 @@ import java.util.Arrays;
 
 class Solution {
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
-        // 初始化
+        // 記錄每一個點的花費金額
+        // 並將src的點設為0
         int[] cost = new int[n];
         Arrays.fill(cost, Integer.MAX_VALUE);
-        // 起始點花費為0
         cost[src] = 0;
-        // 限制停的站數
+        // 由於有限制要在k次內到目的地
+        // 所以只找到k即可
         for (int i = 0; i <= k; i++) {
-            // 暫存cost用來計算最便宜路線
+            // 為了避免去異動到cost[curr]
+            // 所以先用temp去計算本次結果
             int[] temp = Arrays.copyOf(cost, n);
             // 找遍所有可能班機去計算
             for (int[] f : flights) {
@@ -22,14 +24,14 @@ class Solution {
                 if (cost[curr] == Integer.MAX_VALUE) {
                     continue;
                 }
-                // 比較當前路線是否有比上一個路線便宜
+                // 比較所飛到的下個點是否已有其他路線優於本次路線
                 temp[next] = Math.min(temp[next], cost[curr] + price);
             }
-            // 將暫存返還
+            // 計算完後將結果更新至cost
             cost = temp;
         }
 
-        // -1代表沒有任何可能性到終點
+        // 所要到達的目的為Integer.MAX_VALUE代表沒有任何可能性到終點
         return cost[dst] == Integer.MAX_VALUE ? -1 : cost[dst];
     }
 }

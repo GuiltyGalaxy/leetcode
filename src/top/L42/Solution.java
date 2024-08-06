e3721c8b7e906af63632bcc49c473bea8859db6d
Galaxy <kllcoo1300@gmail.com>
2024-08-06 23:40:46 +0800

    2024/08/06 [Top] 42. Trapping Rain Water

--- src/top/L42/Solution.java
+++ src/top/L42/Solution.java
@@ -2,40 +2,36 @@ package top.L42;
 
 class Solution {
     public int trap(int[] height) {
-        /*
-          使用two pointer解法
-          從最左最右往中間逼近
-          利用currHeight來判定向哪邊逼近(因較高的點一定是不動的)
-          在逼近過程中比較前一次高度
-          不斷計算累積量
-         */
-        int left = 0;
-        int rigth = height.length - 1;
+        // 使用two pointer解法
+        // 從最左最右往中間逼近
+        // 利用currHeight來判定向哪邊逼近(因較高的點一定是不動的)
+        // 在逼近過程中比較前一次高度
+        // 不斷計算累積量
+        int L = 0;
+        int R = height.length - 1;
         int sum = 0;
-        int currHeight = Math.max(height[rigth], height[left]);
+        int currHeight = Math.max(height[R], height[L]);
         int lh = 0;
         int rh = 0;
-        while (left < rigth) {
+        while (L < R) {
             // 當前最高值比右邊大
-            if (height[rigth] < currHeight) {
-                // 更新rh
-                rh = Math.max(rh, height[rigth]);
-                // 位移指標
-                rigth--;
+            if (height[R] < currHeight) {
+                rh = Math.max(rh, height[R]);
+                R--;
                 // 代表有積水產生，累加sum
-                if (height[rigth] < rh) {
-                    sum += rh - height[rigth];
+                if (height[R] < rh) {
+                    sum += rh - height[R];
                 }
-            } else if (height[left] <= currHeight) {
+            } else if (height[L] <= currHeight) {
                 // 同理
-                lh = Math.max(lh, height[left]);
-                left++;
-                if (height[left] < lh) {
-                    sum += lh - height[left];
+                lh = Math.max(lh, height[L]);
+                L++;
+                if (height[L] < lh) {
+                    sum += lh - height[L];
                 }
             }
             // 更新目前最高高度
-            currHeight = Math.max(height[rigth], height[left]);
+            currHeight = Math.max(height[R], height[L]);
         }
         return sum;
     }

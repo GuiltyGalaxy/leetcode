dc0127460b1f47cb420683b8e6764dc0dff48cbc
Galaxy <kllcoo1300@gmail.com>
2024-08-08 23:57:49 +0800

    2024/08/08 [Top] 22. Generate Parentheses

--- src/top/L22/Solution.java
+++ src/top/L22/Solution.java
@@ -5,29 +5,32 @@ import java.util.List;
 
 class Solution {
 
-    private final List<String> res = new ArrayList<>();
+    private int maxPairs;
+    private List<String> res;
 
     public List<String> generateParenthesis(int n) {
-        dfs(new StringBuilder(), 0, 0, n);
+        res = new ArrayList<>();
+        maxPairs = n;
+        dfs(new StringBuilder(), 0, 0);
         return res;
     }
 
-    public void dfs(StringBuilder sb, int open, int close, int max) {
+    public void dfs(StringBuilder sb, int open, int close) {
 
         //長度符合則加入答案中
-        if (sb.length() == max * 2) {
+        if (sb.length() == maxPairs * 2) {
             res.add(sb.toString());
             return;
         }
 
-        if (open < max) {
-            dfs(sb.append("("), open + 1, close, max);
+        if (open < maxPairs) {
+            dfs(sb.append("("), open + 1, close);
             sb.deleteCharAt(sb.length() - 1);
         }
         // close < open才可加入close去找
         // 這樣close才會有對應open
         if (close < open) {
-            dfs(sb.append(")"), open, close + 1, max);
+            dfs(sb.append(")"), open, close + 1);
             sb.deleteCharAt(sb.length() - 1);
         }
     }

9fdbb783ef99e128a80c05ad295dea4e674f5d06
Galaxy <kllcoo1300@gmail.com>
2024-08-10 13:36:34 +0800

    2024/08/10 [Top] 153. Find Minimum in Rotated Sorted Array

--- src/top/L153/Solution.java
+++ src/top/L153/Solution.java
@@ -8,8 +8,8 @@ class Solution {
         while (L <= R) {
             int M = (L + R) / 2;
             ans = nums[M];
-            // 有種清況是M已經是最小值
-            // 所以M在範圍內的話都要去判定相鄰兩數是否都比他大
+            // 當M還在範圍內時
+            // 只要他比左右兩邊都小，則代表該數為最小數
             boolean inRange = M > 0 && M < nums.length - 1;
             if (inRange && nums[M - 1] > nums[M] && nums[M] < nums[M + 1]) {
                 return ans;
